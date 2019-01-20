8c7e835583eb8d52c632a60235604b26d7bb100e
Anmol Kasana <kasanaanmol@gmail.com>
2019-01-20 21:53:06 +0530

    add.c file was added

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("sum of the given numbers is",c);
+	return 0;
+}
