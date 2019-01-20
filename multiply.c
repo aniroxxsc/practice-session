367d85c3ccf28b8ce93cc7685c1ca9ddea52f4d0
Anmol Kasana <kasanaanmol@gmail.com>
2019-01-20 22:06:03 +0530

    multiply.c file was added

---
+++ multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("multiplication of given numbers is :",c);
+	return 0;
+
+}
+
