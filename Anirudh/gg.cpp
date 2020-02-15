6427e856875018ba4411ebd3c7c1178109614b25
aniroxxsc <aniroxxtheworld@gmail.com>
2020-02-15 23:12:33 +0530

    added comments in Anirudh/gg

--- Anirudh/gg.cpp
+++ Anirudh/gg.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 int main()
 {
-	int a;
-	char name[30];
-	char c;
-	double d;
-	float f;
-	cout << "enter the integer string charactr double float";
+	int a;	//declaring a variable 
+	
+	char name[30]; // declaring a char variable 
+	char c; //declaring a char variable 
+	double d; // declaring a double variable
+	float f;  // declaring a float variable
+	cout << "enter the integer string charactr double float";  // statement given to screen
 	cin>>a>>name>>c>>d>>f;
 	cout<<a<<name<<c<<d<<f
 	return 0;
 }
+// progam completed
